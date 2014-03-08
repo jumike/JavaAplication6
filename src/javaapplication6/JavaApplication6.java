6c926d8251c60a509dd316742de525b630e28e19
13 <13@I13>
2014-03-08 07:18:06 -0500

    new commit

--- src/javaapplication6/JavaApplication6.java
+++ src/javaapplication6/JavaApplication6.java
@@ -12,11 +12,11 @@ package javaapplication6;
  */
 public class JavaApplication6 {
 
-    /**
+    /** 
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       System.out.println("hello world");
     }
     
 }
